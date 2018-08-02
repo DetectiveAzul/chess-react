c6ca4ee422dda66d62ed28a86a4154ba548859a1
Jaime Lopez <detec.azul@gmail.com>
2018-08-02 14:13:20 +0100

    Client now connect to deployed server

--- src/containers/ChatContainer.js
+++ src/containers/ChatContainer.js
@@ -12,7 +12,7 @@ class ChatContainer extends Component {
       name: null,
       msg: null
     };
-    this.socket = io('https://zeit.co/detectiveazul/server/ainorrigmy:443');
+    this.socket = io('https://server-ehpnaszpzq.now.sh');
     this.socket.on('chat', this.addMessage.bind(this));
 
     this.nameKeyUp = this.nameKeyUp.bind(this);
