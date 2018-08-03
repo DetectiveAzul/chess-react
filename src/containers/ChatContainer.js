e9b007bcf4c3715beb51a57dcc1163513555b688
Jaime Lopez <detec.azul@gmail.com>
2018-08-03 09:34:54 +0100

    Removed remote conection

--- src/containers/ChatContainer.js
+++ src/containers/ChatContainer.js
@@ -12,7 +12,7 @@ class ChatContainer extends Component {
       name: null,
       msg: null
     };
-    this.socket = io('https://server-bkztjzrngb.now.sh/');
+    this.socket = io('localhost:3001');
     this.socket.on('chat', this.addMessage.bind(this));
 
     this.nameKeyUp = this.nameKeyUp.bind(this);
