84623c93c220916f36cad90bcc0cd844ab079d22
Jaime Lopez <detec.azul@gmail.com>
2018-08-03 09:40:47 +0100

    Chess engine started on ChessContainer.js, refactored 'chessMoved() to 'onMoveEnd()'

--- src/containers/ChessContainer.js
+++ src/containers/ChessContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Chessboard from 'react-chessboardjs';
+import Chess from 'chess.js';
 import io from 'socket.io-client';
 
 
@@ -7,31 +8,37 @@ class ChessContainer extends Component {
   constructor() {
     super()
     this.state = {
-      fen: 'start'
+      fen: 'start',
+      game: new Chess()
     };
 
-    this.socket = io('localhost:3001');
-    this.socket.on('chess-received', this.changeBoard.bind(this));
-    this.chessMove = this.chessMove.bind(this);
+    // this.socket = io('localhost:3001');
+    // this.socket.on('chess-received', this.changeBoard.bind(this));
+    this.onMoveEnd = this.onMoveEnd.bind(this);
   };
 
-  changeBoard(newBoard) {
-    this.setState({
-      fen: newBoard
-    });
-    console.log('Chess newBoard received through socket');
-  };
+  //Initial setting will be here
+  componentDidMount() {
 
-  chessMove(oldPos, newPos) {
+  }
+
+  // changeBoard(newBoard) {
+  //   this.setState({
+  //     fen: newBoard
+  //   });
+  //   console.log('Chess newBoard received through socket');
+  // };
+
+  onMoveEnd(oldPos, newPos) {
     console.log('onMoveEnd fired', oldPos, newPos);
-    this.socket.emit('chess-moved', newPos);
+    // this.socket.emit('chess-moved', newPos);
   }
 
   render() {
     return(
       <div className='chess-container'>
         <Chessboard
-          onMoveEnd={this.chessMove}
+          onMoveEnd={this.onMoveEnd}
           fen={this.state.fen}
         />
       </div>
