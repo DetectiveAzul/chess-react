e1c6f0c49fe4f26589ed055f2f50587e18ae8b01
Jaime Lopez <detec.azul@gmail.com>
2018-08-09 10:40:04 +0100

    New server url config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,11 @@
-export default {
-  server: 'http://localhost:3001',
-  serverGame: 'http://localhost:3001/games',
-  serverPlayer: 'http://localhost:3001/players'
-}
-
 // export default {
-//   server: 'https://server-asvhiytppz.now.sh',
-//   serverGame: 'https://server-asvhiytppz.now.sh/games',
-//   serverPlayer: 'https://server-asvhiytppz.now.sh/players'
+//   server: 'http://localhost:3001',
+//   serverGame: 'http://localhost:3001/games',
+//   serverPlayer: 'http://localhost:3001/players'
 // }
+
+export default {
+  server: 'https://server-pahsvduzwm.now.sh',
+  serverGame: 'https://server-pahsvduzwm.now.sh/games',
+  serverPlayer: 'https://server-pahsvduzwm.now.sh/players'
+}
