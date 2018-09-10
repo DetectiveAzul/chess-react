dc0a80c92148dc44e793077d09fec3867ddde0d0
Jaime Lopez <detec.azul@gmail.com>
2018-09-10 10:07:48 +0100

    Preparing github pages deploy

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,12 @@
-export default {
-  client: 'http://localhost:3000',
-  server: 'http://localhost:3001',
-  serverGame: 'http://localhost:3001/games',
-  serverPlayer: 'http://localhost:3001/players'
-}
-
 // export default {
-//   server: 'https://server-pahsvduzwm.now.sh',
-//   serverGame: 'https://server-pahsvduzwm.now.sh/games',
-//   serverPlayer: 'https://server-pahsvduzwm.now.sh/players'
+//   client: 'http://localhost:3000',
+//   server: 'http://localhost:3001',
+//   serverGame: 'http://localhost:3001/games',
+//   serverPlayer: 'http://localhost:3001/players'
 // }
+
+export default {
+  server: 'https://server-bhljkevnge.now.sh',
+  serverGame: 'https://server-bhljkevnge.now.sh',
+  serverPlayer: 'https://server-bhljkevnge.now.sh'
+}
