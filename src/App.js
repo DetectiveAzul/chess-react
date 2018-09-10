2ef60cb8399c1d24d800ea1f806af22164d032d5
Jaime Lopez <detec.azul@gmail.com>
2018-09-10 12:11:11 +0100

    Fixing new game accesing and old game

--- src/App.js
+++ src/App.js
@@ -37,16 +37,16 @@ class App extends Component {
       gamesData.reverse();
         this.setState({
         view: <GameContainer
-          // id={gamesData[0]._id}
-          // fen={gamesData[0].fen}
-          // history={gamesData[0].history}
+          id={gamesData[0]._id}
+          fen={gamesData[0].fen}
+          history={gamesData[0].history}
           finished={false}
           ai={false}
           aiDifficulty='1'
           // Database inversion (MLab)
-          id={gamesData[gamesData.length -1]._id}
-          fen={gamesData[gamesData.length -1].fen}
-          history={gamesData[gamesData.length -1].history}
+          // id={gamesData[gamesData.length -1]._id}
+          // fen={gamesData[gamesData.length -1].fen}
+          // history={gamesData[gamesData.length -1].history}
         />
       });
 
